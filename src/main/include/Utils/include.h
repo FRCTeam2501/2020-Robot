c7a868c7ea81647f3eac4d3a1a243a6a18882481
FRC2501 <frcteam2501@gmail.com>
2020-01-06 16:30:34 -0600

    I code things and they might not work

---
+++ src/main/include/Utils/include.h
@@ -0,0 +1,11 @@
+#pragma once
+#include "frc/WPILib.h"
+#include "ctre/Phoenix.h"
+#include <time.h>
+
+//#include "2501lib.cpp"
+#include "Utils/ports.h"
+#include "Utils/constants.h"
+#include "Utils/buttonmap.h"
+
+#define cout wpi::outs()
