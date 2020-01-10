18a8fa0ee4a469021a84c20e5ce53ad2494da941
FRC2501 <frcteam2501@gmail.com>
2020-01-09 18:56:18 -0600

    OI header done, fixed Parkers stupid ass U

---
+++ src/main/cpp/Drivetrain/ManualDrive.cpp
@@ -0,0 +1,20 @@
+#include "Drivetrain/ManualDrive.h"
+#include "Utils/include.h"
+
+using namespace frc;
+
+ManualDrive::ManualDrive() {
+  // Use addRequirements() here to declare subsystem dependencies.
+}
+
+// Called when the command is initially scheduled.
+void ManualDrive::Initialize() {}
+
+// Called repeatedly when this Command is scheduled to run
+void ManualDrive::Execute() {}
+
+// Called once the command ends or is interrupted.
+void ManualDrive::End(bool interrupted) {}
+
+// Returns true when the command should end.
+bool ManualDrive::IsFinished() { return false; }
--- src/main/cpp/OI/OI.cpp
+++ src/main/cpp/OI/OI.cpp
@@ -1,11 +1,10 @@
 /*----------------------------------------------------------------------------*/
-/* Copyright (c) 2019 FIRST. All Rights Reserved.                             */
+/* Copyright (c) 2018 FIRST. All Rights Reserved.                             */
 /* Open Source Software - may be modified and shared by FRC teams. The code   */
 /* must be accompanied by the FIRST BSD license file in the root directory of */
 /* the project.                                                               */
 /*----------------------------------------------------------------------------*/
 
-#include "commands/ExampleCommand.h"
+#include "OI/OI.h"
 
-ExampleCommand::ExampleCommand(ExampleSubsystem* subsystem)
-    : m_subsystem{subsystem} {}
+OI::OI() {}
