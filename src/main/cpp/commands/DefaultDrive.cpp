e1ea9c3c91af2ead3e57d32850d775b8c5536ddc
TylerSeiford <TylerSeiford@users.noreply.github.com>
2020-01-11 16:43:24 -0600

    Cleaned up stuff, this should be functionally the same for driving

--- src/main/cpp/commands/DefaultDrive.cpp
+++ src/main/cpp/commands/DefaultDrive.cpp
@@ -1,19 +1,12 @@
-/*----------------------------------------------------------------------------*/
-/* Copyright (c) 2019 FIRST. All Rights Reserved.                             */
-/* Open Source Software - may be modified and shared by FRC teams. The code   */
-/* must be accompanied by the FIRST BSD license file in the root directory of */
-/* the project.                                                               */
-/*----------------------------------------------------------------------------*/
-
 #include "commands/DefaultDrive.h"
 
 DefaultDrive::DefaultDrive(DriveSubsystem* subsystem,
-                           std::function<double()> forward,
-                           std::function<double()> rotation)
-    : m_drive{subsystem}, m_forward{forward}, m_rotation{rotation} {
-  AddRequirements({subsystem});
+	std::function<double()> forward,
+	std::function<double()> rotation)
+		: drive{subsystem}, forward{forward}, rotation{rotation} {
+	AddRequirements({subsystem});
 }
 
 void DefaultDrive::Execute() {
-  m_drive->ArcadeDrive(m_forward(), m_rotation());
+	drive->ArcadeDrive(forward(), rotation());	
 }
