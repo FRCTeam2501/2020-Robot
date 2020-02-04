23bfe266bb1ec1d032f5f9d63aa470a9a1768a61
JacobHagberg <139645@isd622.org>
2020-02-03 18:01:53 -0600

    I lied earler, Shooter is now better than Briab's.
    HAHA BRIAB!!!!!!!
    This is because of Tyler's help.

--- src/main/cpp/subsystems/Shooter.cpp
+++ src/main/cpp/subsystems/Shooter.cpp
@@ -5,7 +5,7 @@ using namespace rev;
 Shooter::Shooter(){
 
 spinnyThrowerLeft = new CANSparkMax(CAN::SPINNYTHROWERLEFT,CANSparkMax::MotorType::kBrushless);
-spinnyThrowerRight = new CANSparkMax(CAN::SPINNYTHROWERRIGHT,CANSparkMax::MotorType::kBrushless);
+
     spinnyThrowerLeft->GetPIDController().SetP(kP);
     spinnyThrowerLeft->GetPIDController().SetI(kI);
     spinnyThrowerLeft->GetPIDController().SetD(kD);
@@ -13,12 +13,7 @@ spinnyThrowerRight = new CANSparkMax(CAN::SPINNYTHROWERRIGHT,CANSparkMax::MotorT
     spinnyThrowerLeft->GetPIDController().SetFF(kFF);
     spinnyThrowerLeft->GetPIDController().SetOutputRange(kMinOutput, kMaxOutput);
 
-    spinnyThrowerRight->GetPIDController().SetP(kP);
-    spinnyThrowerRight->GetPIDController().SetI(kI);
-    spinnyThrowerRight->GetPIDController().SetD(kD);
-    spinnyThrowerRight->GetPIDController().SetIZone(kIz);
-    spinnyThrowerRight->GetPIDController().SetFF(kFF);
-    spinnyThrowerRight->GetPIDController().SetOutputRange(kMinOutput, kMaxOutput);
+    spinnyThrowerRight->Follow (*spinnyThrowerLeft);
 }
 
 
