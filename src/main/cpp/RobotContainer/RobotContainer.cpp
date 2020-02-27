1e2122d04025cd2ba62c73f468cf879fac3af727
FRC2501 <frcteam2501@gmail.com>
2020-02-27 16:05:58 -0600

    Added ReverseHopper and Pinz

--- src/main/cpp/RobotContainer/RobotContainer.cpp
+++ src/main/cpp/RobotContainer/RobotContainer.cpp
@@ -145,6 +145,17 @@ RobotContainer::RobotContainer()  {
 		},
 		{ hopper }
 	));
+
+	reverseHopper = new frc2::JoystickButton(controlStick, JOYSTICK::BUTTON_11);
+	reverseHopper->ToggleWhenPressed(new frc2::StartEndCommand(
+		[this] {
+			hopper->UppyWuppy(1.0);
+		},
+		[this] {
+			hopper->UppyWuppy(0.0);
+		},
+		{ hopper }
+	));
 }
 RobotContainer::~RobotContainer(){
 	delete drive;
